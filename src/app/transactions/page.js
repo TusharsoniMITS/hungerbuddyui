cc319e33245ff6b80440db4286e2ab634c5adc21
Tushar soni <tusharsoni@Tushars-MacBook-Air.local>
2026-06-26 00:04:14 +0530

    Fix transactions page

---
+++ src/app/transactions/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TransactionsPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Transactions</h1>
+      <p>Under Development</p>
+    </div>
+  );
+}
