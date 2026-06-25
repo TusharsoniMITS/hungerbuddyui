5500bed24012eb31bbef00b245fc626ad695431c
Tushar soni <tusharsoni@Tushars-MacBook-Air.local>
2026-06-25 23:56:07 +0530

    Fix profile page

---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ProfilePage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Profile Page</h1>
+      <p>Under Development</p>
+    </div>
+  );
+}
